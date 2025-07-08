fd0468cf9bb512189fb54dc47568ab789a469bea
karthicksanthosh <karthick.santhosh1999@gmail.com>
2025-07-08 23:25:55 +0530

    first commit

---
+++ GUV-Intro.py
@@ -0,0 +1,15 @@
+def gcd(m:int,n:int):
+    fm=[]
+    for i in range(1,m+1):
+        if m%i == 0:
+            fm.append(i)
+    fn=[]
+    for j in range(1,n+1):
+        if n%j == 0:
+            fn.append(j)
+    fc=[]
+    for f in fm:
+        if f in fn:
+            fc.append(f)
+    return fc[-1]
+print(gcd(14, 63))
